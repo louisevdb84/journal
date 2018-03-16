0a99d4f1c447e4be703723ec97950c21215a6bb3
Louise van den Berg <louisevdb84@gmail.com>
2018-03-16 09:51:42 +0200

    s

--- controllers/getJournalEntries.js
+++ controllers/getJournalEntries.js
@@ -3,7 +3,7 @@ const handleGetJournalEntries = (req, res, db) => {
   var { username } = req.params;    
   username = "'" + username + "'";
   console.log(username);
-  db('journal').where('username', {username})
+  db('journal').where(username, {username})
       .then(entry => {
         res.json(entry);
         // if (entry.length) {
