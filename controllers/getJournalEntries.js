db434eac5d14d88a5d0c7745bb170a369058c43b
Louise van den Berg <louisevdb84@gmail.com>
2018-03-16 09:48:17 +0200

    d

--- controllers/getJournalEntries.js
+++ controllers/getJournalEntries.js
@@ -3,13 +3,14 @@ const handleGetJournalEntries = (req, res, db) => {
   var { username } = req.params;    
   username = "'" + username + "'";
   console.log(username);
-    db.select('*').from('journal').where('username', {username})
+  db('journal').where('username', {username})
       .then(entry => {
-        if (entry.length) {
-          res.json(entry)
-        } else {
-          res.status(400).json('Not found')
-        }
+        res.json(entry);
+        // if (entry.length) {
+        //   res.json(entry)
+        // } else {
+        //   res.status(400).json('Not found')
+        // }
       })
       .catch(err => res.status(400).json('error getting journalEntries'))
   }
